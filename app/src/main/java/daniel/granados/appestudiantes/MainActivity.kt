8515c1f717e0ec52b9c76525e1287db45390a22d
Daniel Granados <20200008@ricaldone.edu.sv>
2024-04-12 11:25:23 -0600

    Hice que apareciera el menu hamburguesa en todas las paginas

--- app/src/main/java/daniel/granados/appestudiantes/MainActivity.kt
+++ app/src/main/java/daniel/granados/appestudiantes/MainActivity.kt
@@ -38,7 +38,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.fragment_codigos, R.id.fragment_perfil
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
