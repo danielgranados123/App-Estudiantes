1e79fbcb607ee62fe84ecb8f6c5e9076d9e639f1
Daniel Granados <20200008@ricaldone.edu.sv>
2024-04-12 11:11:50 -0600

    Creacion de archivo

---
+++ app/src/main/java/daniel/granados/appestudiantes/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package daniel.granados.appestudiantes.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
